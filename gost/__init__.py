747b88cf260ac1651ff9d24d0fe092f45ad51a21
Felix Heeger <fheeger@posteo.de>
2015-08-01 22:04:15 +0200

    Add GPL

--- gost/__init__.py
+++ gost/__init__.py
@@ -1,3 +1,20 @@
+#Copyright (C) 2015 Felix Heeger
+#
+#This file is part of GOST: GeO Survey Tool.
+#
+#GOST is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#GOST is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with GOST.  If not, see <http://www.gnu.org/licenses/>.
+
 bl_info = {
     "name": "GOST: GeO Survey Tool",
     "author": "Lukas Fischer, Felix Heeger",
