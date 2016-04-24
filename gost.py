0a85e69b3d9402a09145ed1ff0cc15c753bed4e8
Felix Heeger <fheeger@posteo.de>
2016-04-24 20:14:08 +0200

    added logo to the top of the gost app

--- gost.py
+++ gost.py
@@ -17,6 +17,9 @@
 
 
 import sys
+import os
+import time
+
 import bpy
 import bmesh
 from mathutils import Vector
@@ -29,25 +32,81 @@ from PyQt5.QtCore import QTimer, Qt
 from .tachy import TachyConnection, Timeout
 from .util import getSelectedObject, getContext, rad2gon
 
+class MeasureLog(object):
+    knownFormats = ["std", "dat"]
+    
+    def __init__(self, outPath=None, format="std", append=False):
+        self.measurments = []
+        if format not in self.knownFormats:
+            raise ValueError("%s is not a known format" % format)
+        self.format = format
+        if append:
+            mode = "a"
+        else:
+            mode = "w"
+        if outPath is None:
+            outPath = os.path.join(os.path.expanduser("~"),  "%s_gostlog.txt" % time.strftime("%Y-%m-%d_%H-%M-%S"))
+        self.out = open(outPath, mode)
+        
+    def __del__(self):
+        self.out.flush()
+        self.out.close()
+        
+    def addPoint(self, pData):
+        self.measurments.append((time.localtime(), pData))
+        self.out.write(self.formatLine())
+        self.out.flush()
+        
+    def formatLine(self, line=None, lineNr=None, format=None):
+        if line is None:
+            line = self.measurments[-1]
+            lineNr = len(self.measurments)
+        if format is None:
+            format = self.format
+        if format == "std":
+            return time.strftime("%d %b %Y %H:%M:%S") + "\t%(ptid)s\t%(targetEast).3f\t%(targetNorth).3f\t%(targetHeight).3f\n" % line[1]
+        elif format == "dat":
+            return str(lineNr) + "\t%(ptid)s\tX\t%(targetEast).3f\tY\t%(targetNorth).3f\tZ\t%(targetHeight).3f\n" % line[1]
+        else:
+            raise ValueError("%s is not a known format" % format)
+    
+    def writeAll(self, outPath, format=None):
+        with open(outPath, "w") as out:
+            for l, line in enumerate(self.measurments):
+                out.write(self.formatLine(line, l+1, format))
+        
+    
 class QtGostApp(QWidget):
     def __init__(self, parent=None):
         super(QtGostApp, self).__init__(parent)
-        
+        print(os.getcwd())
+        print(__file__)
         self.connectButton = QPushButton("Mit Tachymeter verbinden")
         self.stationButton = QPushButton("Freie Stationierung")
         self.setStationButton = QPushButton("Stationierung auf Punkt")
         self.measurePolyButton = QPushButton("Poly-Linie messen")
         self.settingsButton = QPushButton("Standart Einstellungen")
         self.layoutButton = QPushButton("Neues Ansichtsfenster")
+        self.exportMeasurmentsButton = QPushButton("Messungen Exportieren")
         self.stationButton.setEnabled(False)
         
-        mainLayout = QBoxLayout(2)
-        mainLayout.addWidget(self.connectButton)
-        mainLayout.addWidget(self.stationButton)
-        mainLayout.addWidget(self.setStationButton)
-        mainLayout.addWidget(self.measurePolyButton)
-        mainLayout.addWidget(self.settingsButton)
-        mainLayout.addWidget(self.layoutButton)
+        logo = QLabel()
+        logo.setGeometry(10,10,280,104)
+        logo.setPixmap(QPixmap(os.path.join(os.path.split(__file__)[0], "gostLogo.png")))
+        logo.setContentsMargins(0, 0, 0, 0)
+        
+        buttonLayout = QBoxLayout(QBoxLayout.TopToBottom)
+        buttonLayout.addWidget(self.connectButton)
+        buttonLayout.addWidget(self.stationButton)
+        buttonLayout.addWidget(self.setStationButton)
+        buttonLayout.addWidget(self.measurePolyButton)
+        buttonLayout.addWidget(self.settingsButton)
+        buttonLayout.addWidget(self.layoutButton)
+        buttonLayout.addWidget(self.exportMeasurmentsButton)
+        
+        mainLayout = QBoxLayout(QBoxLayout.TopToBottom, self)
+        mainLayout.addWidget(logo, alignment=Qt.AlignTop)
+        mainLayout.addLayout(buttonLayout)
         self.setLayout(mainLayout)
         
         self.connectButton.clicked.connect(self.openConnectWindow)
@@ -56,11 +115,13 @@ class QtGostApp(QWidget):
         self.setStationButton.clicked.connect(self.openSetStationWindow)
         self.measurePolyButton.clicked.connect(self.measurePoly)
         self.layoutButton.clicked.connect(self.newLayout)
+        self.exportMeasurmentsButton.clicked.connect(self.exportMeasurments)
         
         self.connection = TachyConnection()
+        self.log = MeasureLog(format="dat")
         
         self.setWindowTitle("GeO Survey Tool - GOST")
-        self.setWindowIcon(QIcon('gost.png'))  
+        self.setWindowIcon(QIcon(os.path.join(os.path.split(__file__)[0], "gost.png")))  
         self.setGeometry(10, 30, 300, 600)
     
     
@@ -173,7 +234,9 @@ class QtGostApp(QWidget):
         self.mes = None
         
         
-
+    def exportMeasurments(self):
+        exportWindow = QtGostExportWindow(self)
+        exportWindow.show()
     
 class QtGostSettings(QDialog):
     
@@ -553,6 +616,7 @@ class QtWaitForPolyLine(QDialog):
     def pollMeasurement(self):
         try:
             measurement = self.parentWidget().connection.readMeasurement()
+            self.parentWidget().log.addPoint(measurement)
         except Timeout:
             pass
         else:
@@ -664,6 +728,7 @@ class QtWaitForMeasurement(QMessageBox):
 
     def pollTachy(self):
         self.data = self.parentWidget().parentWidget().connection.readMeasurement()
+        self.parentWidget().parentWidget().log.addPoint(self.data)
         self.timer.stop()
         self.accept()
  
@@ -700,7 +765,33 @@ class QtSetStation(QDialog):
         a = float(self.angleField.text())
         self.parentWidget().connection.setStation(p, a)
         super(QtSetStation, self).accept()
- 
+
+class QtGostExportWindow(QDialog):
+    def __init__(self, parent=None):
+        super(QtGostExportWindow, self).__init__(parent)
+        mainLayout = QGridLayout()
+        self.setLayout(mainLayout)
+        
+        self.selectFormat = QComboBox(self)
+        for format in self.parentWidget().log.knownFormats:
+            self.selectFormat.addItem(format)
+        
+        self.saveButton = QPushButton("Speichern")
+        
+        mainLayout.addWidget(QLabel("Format auswählen:"), 0, 0)
+        mainLayout.addWidget(self.selectFormat, 0, 1)
+        mainLayout.addWidget(self.saveButton, 1,1)
+        self.saveButton.clicked.connect(self.save)
+
+    def save(self):
+        fileName = QFileDialog.getSaveFileName(self, "Messungen exporiteren", os.path.expanduser("~"))[0]
+        print(fileName)
+        if len(fileName) == 0:
+            self.reject()
+        else:
+            self.parentWidget().log.writeAll(fileName, self.selectFormat.currentText())
+            self.accept()
+        
 # class QtNivel(QDialog):
     # def __init__(self, parent=None):
         # super(QtNivel, self).__init__(parent)
